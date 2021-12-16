abf71440f6d638ca7efd2796a7637795e14ce74b
SamuelYudiGunawan <81005090+SamuelYudiGunawan@users.noreply.github.com>
2021-12-16 16:48:35 +0700

    initial commit

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,16 +1,16 @@
 @extends('errors.layout')
 
 @php
-  $error_number = 404;
+	$error_number = 500;
 @endphp
 
 @section('title')
-  Page not found.
+	It's not you, it's me.
 @endsection
 
 @section('description')
-  @php
-    $default_error_message = "Please <a href='javascript:history.back()''>go back</a> or return to <a href='".url('')."'>our homepage</a>.";
-  @endphp
-  {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
+	@php
+	  $default_error_message = "An internal server error has occurred. If the error persists please contact the development team.";
+	@endphp
+	{!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
 @endsection
